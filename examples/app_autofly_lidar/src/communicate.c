efd53163689b2a79bf5e7d02549e1c1e703cc525
Raven <raven.xu98@gmail.com>
2023-04-09 00:50:27 +0800

    refactor app-autofly-lidar

--- examples/app_autofly_lidar/src/communicate.c
+++ examples/app_autofly_lidar/src/communicate.c
@@ -5,16 +5,12 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
-
 #include "radiolink.h"
 #include "configblock.h"
-
-
 #include "debug.h"
-
-#include"radiolink.h"
 #include "cpx_internal_router.h"
 #include "cpx_external_router.h"
+
 #include "communicate.h"
 
 void P2PCallbackHandler(P2PPacket *p)
@@ -30,12 +26,14 @@ void P2PCallbackHandler(P2PPacket *p)
     DEBUG_PRINT("[RSSI: -%d dBm] P2PMsg from:%d,Point1: (%d,%d,%d), Sent to Ad\n", rssi, other_id, msg[0].x,msg[0].y,msg[0].z);
 }
 
-void ListeningInit(){
+void ListeningInit()
+{
     // Register the callback function so that the CF can receive packets as well.
     p2pRegisterCB(P2PCallbackHandler);
 }
 
-bool SendReq(coordinate_t* coords,ReqType mode,uint16_t seq){
+bool SendReq(coordinate_t* coords,ReqType mode,uint16_t seq)
+{
 
     // Initialize the p2p packet
     static P2PPacket packet;
@@ -58,3 +56,59 @@ bool SendReq(coordinate_t* coords,ReqType mode,uint16_t seq){
     return radiolinkSendP2PPacketBroadcast(&packet);
 }
 
+bool sendMappingRequest(coordinate_pair_t* mappingRequestPayloadPtr, uint8_t mappingRequestPayloadLength, uint16_t mappingRequestSeq) 
+{
+    // Initialize the p2p packet
+    static P2PPacket packet;
+    packet.port = 0x00;
+    // Get the current crazyflie id
+    uint64_t address = configblockGetRadioAddress();
+    uint8_t uavId = (uint8_t)((address) & 0x00000000ff);
+    // Assemble the packet
+    packet.data[0] = uavId;
+    packet.data[1] = MappingReq;
+    packet.data[2] = mappingRequestSeq;
+    memcpy(&packet.data[3], mappingRequestPayloadPtr, sizeof(coordinate_pair_t) * mappingRequestPayloadLength);
+    // 1b for uavId, 2b for mappingRequestSeq, 1b for mappingRequestPayloadLength, 12b for each coordinate_pair_t
+    packet.size = sizeof(uavId) + sizeof(mappingRequestSeq) + sizeof(mappingRequestPayloadLength) + sizeof(coordinate_pair_t)*mappingRequestPayloadLength;
+    // Send the P2P packet
+    return radiolinkSendP2PPacketBroadcast(&packet);
+}
+
+bool sendExploreRequest(coordinate_t* exploreRequestPayloadPtr, uint16_t exploreRequestSeq)
+{
+    // Initialize the p2p packet
+    static P2PPacket packet;
+    packet.port = 0x00;
+    // Get the current crazyflie id
+    uint64_t address = configblockGetRadioAddress();
+    uint8_t uavId = (uint8_t)((address) & 0x00000000ff);
+    // Assemble the packet
+    packet.data[0] = uavId;
+    packet.data[1] = ExploreReq;
+    packet.data[2] = exploreRequestSeq;
+    memcpy(&packet.data[3], exploreRequestPayloadPtr, sizeof(coordinate_t));
+    // 1b for uavId, 2b for exploreRequestSeq, 6b for coordinate_t
+    packet.size = sizeof(uavId) + sizeof(exploreRequestSeq) + sizeof(coordinate_t);
+    // Send the P2P packet
+    return radiolinkSendP2PPacketBroadcast(&packet);
+}
+
+bool sendPathRequest(coordinate_pair_t* pathRequestPayloadPtr, uint16_t pathRequestSeq)
+{
+    // Initialize the p2p packet
+    static P2PPacket packet;
+    packet.port = 0x00;
+    // Get the current crazyflie id
+    uint64_t address = configblockGetRadioAddress();
+    uint8_t uavId = (uint8_t)((address) & 0x00000000ff);
+    // Assemble the packet
+    packet.data[0] = uavId;
+    packet.data[1] = PathReq;
+    packet.data[2] = pathRequestSeq;
+    memcpy(&packet.data[3], pathRequestPayloadPtr, sizeof(coordinate_pair_t));
+    // 1b for uavId, 2b for pathRequestSeq, 12b for coordinate_pair_t
+    packet.size = sizeof(uavId) + sizeof(pathRequestSeq) + sizeof(coordinate_pair_t);
+    // Send the P2P packet
+    return radiolinkSendP2PPacketBroadcast(&packet);
+}
--- examples/app_autofly_lidar/src/communicate.h
+++ examples/app_autofly_lidar/src/communicate.h
@@ -1,11 +1,13 @@
 #ifndef __COMMUNICATE_H__
 #define __COMMUNICATE_H__
 #define DEBUG_MODULE "P2P"
+#include "config_autofly.h"
 typedef enum {
     ExploreReq = 1,
     PathReq = 2,
     MappingReq = 5
 } ReqType;
+
 typedef struct
 {
     uint16_t x;
@@ -13,6 +15,15 @@ typedef struct
     uint16_t z;
 } coordinate_t;
 
+typedef struct
+{
+    coordinate_t startPoint;
+    coordinate_t endPoint;
+} coordinate_pair_t;
+
 void ListeningInit();
-bool SendReq(coordinate_t* coords,ReqType mode,uint16_t seq);
+bool SendReq(coordinate_t* coords, ReqType mode, uint16_t seq);
+bool sendMappingRequest(coordinate_pair_t* mappingRequestPayloadPtr, uint8_t mappingRequestPayloadLength, uint16_t mappingRequestSeq);
+bool sendExploreRequest(coordinate_t* exploreRequestPayloadPtr, uint16_t exploreRequestSeq);
+bool sendPathRequest(coordinate_pair_t* pathRequestPayloadPtr, uint16_t pathRequestSeq);
 #endif
